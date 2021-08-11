4db51cfb24cb02fd3b001915669eea92ea190b92
HeeSu Jung <njhsm1@gmail.com>
2021-08-12 03:06:21 +0900

    second commit

--- practice/views/main_views.py
+++ practice/views/main_views.py
@@ -1,13 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from practice.models import Question
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html', question_list=question_list)
+    return redirect(url_for('question._list'))
+    #question_list = Question.query.order_by(Question.create_date.desc())
+    #return render_template('question/question_list.html', question_list=question_list)
 
 @bp.route('/hello')
 def hello_world():
     return 'Hello World'
+
+@bp.route('/detail/<int:question_id>')
+def detail(question_id):
+    question = Question.query.get_or_404(question_id)
+    return render_template('question/question_detail.html', question=question)
